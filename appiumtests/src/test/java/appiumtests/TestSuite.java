d8b15ea8ff4db3cd10262d21234f3583946560ff
caroline lavergne <caroline.lavergne.partner@decathlon.com>
2021-04-03 20:06:32 +0200

    update

--- appiumtests/src/test/java/appiumtests/TestSuite.java
+++ appiumtests/src/test/java/appiumtests/TestSuite.java
@@ -3,7 +3,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
-@SuiteClasses({JenkinsTest.class})
+@SuiteClasses({JenkinsTest.class, JenkinsTest2.class})
 public class TestSuite {
 
 }
